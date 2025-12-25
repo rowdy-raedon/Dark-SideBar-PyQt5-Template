3674f4e7e98621d72d5840e55a08d88c9552ae06
raedon <rowdyraedon@gmail.com>
2025-12-25 00:46:34 -0500

    Refactor utility functions in core/utils.py to improve path handling for icons and stylesheets

--- core/utils.py
+++ core/utils.py
@@ -2,9 +2,15 @@ from PyQt5.QtCore import Qt, QPoint
 from PyQt5.QtGui import QIcon
 import os
 
+def _project_root():
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+def _qss_path(path):
+    return path.replace("\\", "/")
+
 def get_icon(name):
     """Load and return an icon from the resources directory."""
-    icon_path = os.path.join("resources", "icons", f"{name}.svg")
+    icon_path = os.path.join(_project_root(), "resources", "icons", f"{name}.svg")
     if not os.path.exists(icon_path):
         # Return an empty icon if the file doesn't exist
         return QIcon()
@@ -12,8 +18,15 @@ def get_icon(name):
 
 def load_stylesheet(qss_file):
     """Load and return the contents of a QSS stylesheet file."""
+    if not os.path.isabs(qss_file):
+        qss_file = os.path.join(_project_root(), qss_file)
     with open(qss_file, 'r') as f:
-        return f.read()
+        style = f.read()
+    res_dir = _qss_path(os.path.join(_project_root(), "resources"))
+    style = style.replace("url(resources/", f"url({res_dir}/")
+    style = style.replace("url(\"resources/", f"url(\"{res_dir}/")
+    style = style.replace("url('resources/", f"url('{res_dir}/")
+    return style
 
 class WindowDragger:
     """Helper class for implementing window dragging."""
